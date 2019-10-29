18f9a47d6774b654afd6c5781dbcfa8e7010cbb7
bennsimon <botieno@ona.io>
2019-10-29 16:13:33 +0300

    fixed failing test

--- opensrp-opd/src/test/java/org/smartregister/opd/utils/OpdUtilsTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/utils/OpdUtilsTest.java
@@ -1,7 +1,6 @@
 package org.smartregister.opd.utils;
 
 import org.jeasy.rules.api.Facts;
-import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,9 +27,9 @@ public class OpdUtilsTest {
     }
 
     @Test
-    public void convertStringToDate(){
+    public void convertStringToDate() {
         Date date = OpdUtils.convertStringToDate(OpdConstants.DateFormat.YYYY_MM_DD_HH_MM_SS, "2019-10-28 18:09:49");
-        Assert.assertNull(date);
+        Assert.assertNotNull(date);
     }
 
 }
