e198cb552e50bfaf52c89f46c2f53122a8945561
bennsimon <botieno@ona.io>
2019-10-29 16:22:30 +0300

    fix failing test

--- opensrp-opd/src/test/java/org/smartregister/opd/presenter/BaseOpdRegisterActivityPresenterTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/presenter/BaseOpdRegisterActivityPresenterTest.java
@@ -2,12 +2,20 @@ package org.smartregister.opd.presenter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.robolectric.util.ReflectionHelpers;
+import org.smartregister.opd.OpdLibrary;
 import org.smartregister.opd.contract.OpdRegisterActivityContract;
+import org.smartregister.opd.repository.OpdDiagnosisAndTreatmentFormRepository;
 
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
@@ -16,12 +24,27 @@ import java.util.HashMap;
  * Created by Ephraim Kigamba - ekigamba@ona.io on 2019-10-24
  */
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(OpdLibrary.class)
 public class BaseOpdRegisterActivityPresenterTest {
 
+    @Mock
+    private OpdLibrary opdLibrary;
+
+    @Mock
+    private OpdDiagnosisAndTreatmentFormRepository opdDiagnosisAndTreatmentFormRepository;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void startFormShouldPassEntityTableAndBaseEntityIdToActivity() throws JSONException {
+        PowerMockito.mockStatic(OpdLibrary.class);
+        PowerMockito.when(OpdLibrary.getInstance()).thenReturn(opdLibrary);
+        PowerMockito.when(opdLibrary.getOpdDiagnosisAndTreatmentFormRepository()).thenReturn(opdDiagnosisAndTreatmentFormRepository);
+
         OpdRegisterActivityContract.View view = Mockito.mock(OpdRegisterActivityContract.View.class);
         OpdRegisterActivityContract.Model model = Mockito.mock(OpdRegisterActivityContract.Model.class);
 
