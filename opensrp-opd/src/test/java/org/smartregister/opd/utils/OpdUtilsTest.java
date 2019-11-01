f2280fe93adfbb61e28cc8d8944f6a6ce2d5bfab
Elly Kitoto <junkmailstoelly@gmail.com>
2019-11-01 17:41:02 +0300

    Optimize imports

--- opensrp-opd/src/test/java/org/smartregister/opd/utils/OpdUtilsTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/utils/OpdUtilsTest.java
@@ -3,7 +3,6 @@ package org.smartregister.opd.utils;
 import android.content.Intent;
 
 import org.jeasy.rules.api.Facts;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
