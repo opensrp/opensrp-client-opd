c844957310e4aece7d8befad7979c6173df4f920
bennsimon <botieno@ona.io>
2019-12-19 10:18:50 +0300

    codacy fix

--- opensrp-opd/src/main/java/org/smartregister/opd/widgets/OpdRepeatingGroup.java
+++ opensrp-opd/src/main/java/org/smartregister/opd/widgets/OpdRepeatingGroup.java
@@ -3,8 +3,6 @@ package org.smartregister.opd.widgets;
 import android.content.Context;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.vijay.jsonwizard.fragments.JsonFormFragment;
@@ -19,28 +17,22 @@ import java.util.List;
 public class OpdRepeatingGroup extends RepeatingGroupFactory {
 
 
-    int count = 1;
-
-    @Override
-    protected void addOnDoneAction(TextView textView) {
-        super.addOnDoneAction(textView);
-    }
+    private int count = 1;
 
     @Override
     public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener, boolean popup) throws Exception {
         List<View> viewList = super.getViewsFromJson(stepName, context, formFragment, jsonObject, listener, popup);
-        Button repeating_group_intemediate_btn = viewList.get(0).findViewById(R.id.repeating_group_intermediate_btn);
-        MaterialEditText referenceEditText = viewList.get(0).findViewById(R.id.reference_edit_text);
-        repeating_group_intemediate_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        View root = viewList.isEmpty() ? null : viewList.get(0);
+        if(root != null) {
+            Button repeatingGroupIntermediateBtn = viewList.get(0).findViewById(R.id.repeating_group_intermediate_btn);
+            MaterialEditText referenceEditText = viewList.get(0).findViewById(R.id.reference_edit_text);
+            repeatingGroupIntermediateBtn.setOnClickListener(v -> {
                 referenceEditText.setText(String.valueOf(++count));
                 addOnDoneAction(referenceEditText);
-            }
-        });
+            });
+        }
         return viewList;
     }
-
 
 
 }
