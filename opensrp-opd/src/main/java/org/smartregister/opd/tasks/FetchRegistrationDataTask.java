5f0fa9c960c9a0f08cbc596a596e4354e5593e80
bennsimon <botieno@ona.io>
2020-01-17 14:34:14 +0300

    prepare for v0.2.6

--- opensrp-opd/src/main/java/org/smartregister/opd/tasks/FetchRegistrationDataTask.java
+++ opensrp-opd/src/main/java/org/smartregister/opd/tasks/FetchRegistrationDataTask.java
@@ -1,6 +1,5 @@
 package org.smartregister.opd.tasks;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
