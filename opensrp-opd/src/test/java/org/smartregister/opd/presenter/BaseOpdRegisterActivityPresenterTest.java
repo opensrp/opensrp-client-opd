8313ecb134e9a7e5c5740688ee114898e879a738
bennsimon <botieno@ona.io>
2020-02-12 18:00:33 +0300

    fix tests

--- opensrp-opd/src/test/java/org/smartregister/opd/presenter/BaseOpdRegisterActivityPresenterTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/presenter/BaseOpdRegisterActivityPresenterTest.java
@@ -47,7 +47,7 @@ public class BaseOpdRegisterActivityPresenterTest {
         OpdRegisterActivityContract.View view = Mockito.mock(OpdRegisterActivityContract.View.class);
         OpdRegisterActivityContract.Model model = Mockito.mock(OpdRegisterActivityContract.Model.class);
 
-        BaseOpdRegisterActivityPresenter baseOpdRegisterActivityPresenter = Mockito.spy(new OpdRegisterActivityPresenter(view, model));
+        BaseOpdRegisterActivityPresenter baseOpdRegisterActivityPresenter = new OpdRegisterActivityPresenter(view, model);
 
         Mockito.doReturn(new JSONObject()).when(model).getFormAsJson(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.nullable(HashMap.class));
 
