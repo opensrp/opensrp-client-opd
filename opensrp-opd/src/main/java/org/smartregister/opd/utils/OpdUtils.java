d6e593899a192f1793d3ee9eac6d810375a88f1d
bennsimon <botieno@ona.io>
2020-09-04 18:27:09 +0300

    remove update of key in repeating grp

--- opensrp-opd/src/main/java/org/smartregister/opd/utils/OpdUtils.java
+++ opensrp-opd/src/main/java/org/smartregister/opd/utils/OpdUtils.java
@@ -277,7 +277,6 @@ public class OpdUtils extends Utils {
                     fieldKey = fieldKey.substring(0, fieldKey.lastIndexOf("_"));
                     if (keysArrayList.contains(fieldKey) && StringUtils.isNotBlank(fieldValue) && !fieldValue.equals(OpdConstants.TYPE_OF_TEXT_LABEL)) {
                         String fieldKeyId = valueField.optString(JsonFormConstants.KEY).substring(fieldKey.length() + 1);
-                        valueField.put(JsonFormConstants.KEY, fieldKey);
                         HashMap<String, String> hashMap = repeatingGroupMap.get(fieldKeyId) == null ? new HashMap<>() : repeatingGroupMap.get(fieldKeyId);
                         hashMap.put(fieldKey, fieldValue);
                         repeatingGroupMap.put(fieldKeyId, hashMap);
