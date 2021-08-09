050ab3a4859c39a17a3fb409c420fd88c37a5bf8
Hamza <hamza.khan@venturedive.com>
2021-08-09 12:05:43 +0500

    Update native-forms to v2.1.2

--- opensrp-opd/src/main/java/org/smartregister/opd/widgets/OpdMultiSelectDrugPicker.java
+++ opensrp-opd/src/main/java/org/smartregister/opd/widgets/OpdMultiSelectDrugPicker.java
@@ -85,7 +85,7 @@ public class OpdMultiSelectDrugPicker extends MultiSelectListFactory implements 
 
                 writeAdditionalDetails(duration, dosage, frequency, multiSelectItem);
 
-                writeToForm();
+                writeToForm(currentAdapterKey);
 
                 alertDialog.dismiss();
 
