61fa63b9f3b1e632b33bee3f78ccb2f9269412b4
Hamza <hamza.khan@venturedive.com>
2021-08-11 14:45:48 +0500

    Fix failing tests

--- opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileVisitFragmentPresenterTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileVisitFragmentPresenterTest.java
@@ -1,22 +1,35 @@
 package org.smartregister.opd.presenter;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.smartregister.opd.BaseTest;
+import org.smartregister.opd.fragment.NewOpdProfileVisitsFragment;
+import org.smartregister.util.GenericInteractor;
 
 @RunWith(RobolectricTestRunner.class)
 public class NewOpdProfileVisitFragmentPresenterTest extends BaseTest {
     
-//    private NewOpdProfileVisitsFragmentPresenter presenter;
-//    private NewOpdProfileVisitsFragment view;
+    private NewOpdProfileVisitsFragmentPresenter presenter;
 
     @Before
     public void setUp() {
-        // TODO: Add tests
-//        view  = NewOpdProfileVisitsFragment.newInstance(null);
-//        presenter = (NewOpdProfileVisitsFragmentPresenter) Mockito.spy(new NewOpdProfileVisitsFragmentPresenter().with(view));
+        NewOpdProfileVisitsFragment view = NewOpdProfileVisitsFragment.newInstance(null);
+        presenter = (NewOpdProfileVisitsFragmentPresenter) Mockito.spy(new NewOpdProfileVisitsFragmentPresenter().with(view));
     }
-    
+
+    @Test
+    public void getCallableInteractShouldReturnGenericInteract() {
+        Assert.assertEquals(GenericInteractor.class, presenter.getCallableInteractor().getClass());
+    }
+
+
+    @Test
+    public void getViewShouldReturnNewOpdProfileVisitsFragment() {
+        Assert.assertEquals(NewOpdProfileVisitsFragment.class, presenter.getView().getClass());
+    }
     
 }
