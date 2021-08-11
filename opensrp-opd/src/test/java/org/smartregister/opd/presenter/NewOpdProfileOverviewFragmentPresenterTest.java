8d48faf48a742d7c9ac03753dc2275e10b072555
Hamza <hamza.khan@venturedive.com>
2021-08-11 12:48:00 +0500

    Fix coDcy errors

--- opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileOverviewFragmentPresenterTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileOverviewFragmentPresenterTest.java
@@ -1,30 +1,19 @@
 package org.smartregister.opd.presenter;
 
-import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.util.ReflectionHelpers;
-import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.opd.BaseTest;
 import org.smartregister.opd.fragment.NewOpdProfileOverviewFragment;
-import org.smartregister.util.JsonFormUtils;
-
-import java.util.HashMap;
 
 @RunWith(RobolectricTestRunner.class)
 public class NewOpdProfileOverviewFragmentPresenterTest extends BaseTest {
-    
-    private NewOpdProfileOverviewFragmentPresenter presenter;
-    private NewOpdProfileOverviewFragment view;
 
     @Before
     public void setUp() {
-        view  = NewOpdProfileOverviewFragment.newInstance(null);
-        presenter = (NewOpdProfileOverviewFragmentPresenter) Mockito.spy(new NewOpdProfileOverviewFragmentPresenter().with(view));
+        NewOpdProfileOverviewFragment view = NewOpdProfileOverviewFragment.newInstance(null);
+        NewOpdProfileOverviewFragmentPresenter presenter = (NewOpdProfileOverviewFragmentPresenter) Mockito.spy(new NewOpdProfileOverviewFragmentPresenter().with(view));
     }
     
     
--- opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileVisitFragmentPresenterTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileVisitFragmentPresenterTest.java
@@ -1,19 +1,11 @@
 package org.smartregister.opd.presenter;
 
-import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.util.ReflectionHelpers;
-import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.opd.BaseTest;
 import org.smartregister.opd.fragment.NewOpdProfileVisitsFragment;
-import org.smartregister.util.JsonFormUtils;
-
-import java.util.HashMap;
 
 @RunWith(RobolectricTestRunner.class)
 public class NewOpdProfileVisitFragmentPresenterTest extends BaseTest {
