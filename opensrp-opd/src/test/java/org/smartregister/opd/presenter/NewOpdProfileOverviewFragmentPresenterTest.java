6bec9b7aafd48788d595bfac94d1f1fdd31553dd
Hamza <hamza.khan@venturedive.com>
2021-08-11 12:56:49 +0500

    Fix codacy eroors 2

--- opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileOverviewFragmentPresenterTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileOverviewFragmentPresenterTest.java
@@ -2,18 +2,20 @@ package org.smartregister.opd.presenter;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.smartregister.opd.BaseTest;
-import org.smartregister.opd.fragment.NewOpdProfileOverviewFragment;
 
 @RunWith(RobolectricTestRunner.class)
 public class NewOpdProfileOverviewFragmentPresenterTest extends BaseTest {
 
+//    private NewOpdProfileOverviewFragment view;
+//    private NewOpdProfileOverviewFragmentPresenter presenter
+    
     @Before
     public void setUp() {
-        NewOpdProfileOverviewFragment view = NewOpdProfileOverviewFragment.newInstance(null);
-        NewOpdProfileOverviewFragmentPresenter presenter = (NewOpdProfileOverviewFragmentPresenter) Mockito.spy(new NewOpdProfileOverviewFragmentPresenter().with(view));
+        // TODO:  Add tests
+//        view = NewOpdProfileOverviewFragment.newInstance(null);
+//        presenter = (NewOpdProfileOverviewFragmentPresenter) Mockito.spy(new NewOpdProfileOverviewFragmentPresenter().with(view));
     }
     
     
--- opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileVisitFragmentPresenterTest.java
+++ opensrp-opd/src/test/java/org/smartregister/opd/presenter/NewOpdProfileVisitFragmentPresenterTest.java
@@ -2,21 +2,20 @@ package org.smartregister.opd.presenter;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 import org.smartregister.opd.BaseTest;
-import org.smartregister.opd.fragment.NewOpdProfileVisitsFragment;
 
 @RunWith(RobolectricTestRunner.class)
 public class NewOpdProfileVisitFragmentPresenterTest extends BaseTest {
     
-    private NewOpdProfileVisitsFragmentPresenter presenter;
-    private NewOpdProfileVisitsFragment view;
+//    private NewOpdProfileVisitsFragmentPresenter presenter;
+//    private NewOpdProfileVisitsFragment view;
 
     @Before
     public void setUp() {
-        view  = NewOpdProfileVisitsFragment.newInstance(null);
-        presenter = (NewOpdProfileVisitsFragmentPresenter) Mockito.spy(new NewOpdProfileVisitsFragmentPresenter().with(view));
+        // TODO: Add tests
+//        view  = NewOpdProfileVisitsFragment.newInstance(null);
+//        presenter = (NewOpdProfileVisitsFragmentPresenter) Mockito.spy(new NewOpdProfileVisitsFragmentPresenter().with(view));
     }
     
     
